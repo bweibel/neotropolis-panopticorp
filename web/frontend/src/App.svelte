e3dbf8ef293a0ee12e86e8372e5a050ebda146b9
Ben Weibel <weibel.b@gmail.com>
2026-02-11 17:54:59 -0800

    Add CRT effects

--- web/frontend/src/App.svelte
+++ web/frontend/src/App.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
   import Hero from './components/Hero.svelte';
   import Footer from './components/Footer.svelte';
+  import CrtOverlay from './components/CrtOverlay.svelte';
 </script>
 
 <main>
@@ -10,10 +11,10 @@
     <p>Panopticorp delivers integrated monitoring, predictive risk analysis, and operational coordination everywhere that people live, work, play, and move. We partner with law enforcement, but we don’t like to think of ourselves as police – more like a big brother. We’re here to protect and serve our clientele with the authority and care you need to finally take a deep breath and relax.</p>
     <p>From Portugal to Pax-Panama, the Pacific Aquicolonies to Petrozavodskgrad, the political to the personal: Panopticorp is there. Panopticorp is everywhere.</p>
     <h3>Pervasive. Persistent. Penetrative. Panopticorp.</h3>
-
   </div>
   <Footer />
 </main>
+<CrtOverlay />
 
 <style>
   .mission {
---
+++ web/frontend/src/components/CrtOverlay.svelte
@@ -0,0 +1,29 @@
+
+<div class="crt-overlay"></div>
+
+<style>
+    .crt-overlay {
+        background: url(/images/rgb-bg.jpg);
+        background-color: var(--color-dark);
+        background-size: 16px;
+        background-blend-mode: multiply;
+        animation: crt-overlay-movement 1s steps(100) infinite;
+        position: fixed;
+        top: 0;
+        bottom: 0;
+        left: 0;
+        right: 0;
+        z-index: -1;
+    }
+
+    @keyframes crt-overlay-movement {
+        0%,100% {
+            opacity: 0.4;
+        }
+
+        50% {
+            opacity: 0.3;
+        }
+    }
+
+</style>
