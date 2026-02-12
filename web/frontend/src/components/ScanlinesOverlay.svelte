9c545ba29ef7257cdf69b1b72c2409dc976f132f
Ben Weibel <weibel.b@gmail.com>
2026-02-11 19:08:49 -0800

    Add repeated scanlines image

--- web/frontend/src/components/ScanlinesOverlay.svelte
+++ web/frontend/src/components/ScanlinesOverlay.svelte
@@ -3,26 +3,26 @@
 
 <style>
     .scanlines-overlay {
-        background: url(/images/scanlines.jpg);
-        background-size: 100%;
-        animation: scanlines-overlay-flicker .1s steps(10) infinite;
+        background: url(/images/scanline.png);
+        background-size: 10px;
+        animation: scanlines-overlay-flicker .2s steps(10) infinite;
         position: fixed;
         top: 0;
         bottom: 0;
         left: 0;
         right: 0;
         z-index: 100;
-        opacity: 0.01;
+        opacity: 1;
         pointer-events: none;
     }
 
     @keyframes scanlines-overlay-flicker {
         0%,100% {
-            opacity: 0.01;
+            opacity: 0.05;
         }
 
-        75% {
-            opacity: 0.02;
+        50% {
+            opacity: 0.075;
         }
     }
 
